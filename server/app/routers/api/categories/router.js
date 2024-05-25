afa8215001b0d9ab299886c04d5a73c32a23249f
Dino Saure <lejeune.chris@protonmail.com>
2024-05-25 17:29:19 +0200

    Added categories / router and actions

--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -1,18 +1,21 @@
 const express = require("express");
 
+
 const router = express.Router();
 
+
 /* ************************************************************************* */
+
 // Define Your API Routes Here
+
 /* ************************************************************************* */
 
-// Import item-related actions
-const { browse, read } = require("../../../controllers/programActions");
+const { browse, read } = require("../../../controllers/categoryActions")
 
-// Route to get a list of programs
 router.get("/", browse);
 
-// Route to get one program
 router.get("/:id", read);
+
+/* ************************************************************************* */
 
 module.exports = router;
