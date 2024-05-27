4910fd098ebe154669dfe00b3d84fe3f6c2897c3
Dino Saure <lejeune.chris@protonmail.com>
2024-05-27 08:33:07 +0200

    Program & category DB created

--- server/index.js
+++ server/index.js
@@ -1,18 +1,50 @@
-// Load environment variables from .env file
 require("dotenv").config();
-// Check database connection
-// Note: This is optional and can be removed if the database connection
-// is not required when starting the application
-require("./database/client").checkConnection();
-// Import the Express application from app/config.js
-const app = require("./app/config");
-// Get the port from the environment variables
-const port = process.env.APP_PORT;
-// Start the server and listen on the specified port
-app
-  .listen(port, () => {
-    console.info(`Server is listening on port ${port}`);
-  })
-  .on("error", (err) => {
-    console.error("Error:", err.message);
-  });
+
+const mysql = require("mysql2/promise");
+
+const createDB = async () => {
+  try {
+    // Create a specific connection to the database
+
+    const databaseClient = await mysql.createConnection({
+      host: "localhost",
+      port: "3306",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+
+    // Drop the existing database if it exists
+
+    await databaseClient.query("DROP DATABASE IF EXISTS wildseries");
+
+    // Create a new database with the specified name
+
+    await databaseClient.query("CREATE DATABASE wildseries");
+
+    // Switch to the newly created database
+
+    await databaseClient.query("USE wildseries");
+
+    // Execute the SQL statements to update the database schema
+
+    await databaseClient.query(`CREATE TABLE program(
+
+      id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+
+      title VARCHAR(100) NOT NULL
+
+    )`);
+
+    // Close the database connection
+
+    databaseClient.end();
+
+    console.info("wildseries updated 🆙");
+  } catch (err) {
+    console.error("Error updating the database:", err.message, err.stack);
+  }
+};
+
+// Run the createDB function
+
+createDB();
